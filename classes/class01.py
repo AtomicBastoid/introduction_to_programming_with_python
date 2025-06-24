9c96997a4cdf378ff0129071217e27b501b03e83
Eishal Keshwani <keshwanieishal001@gmail.com>
2025-06-24 13:10:19 +0500

    first commit

---
+++ classes/class01.py
@@ -0,0 +1,42 @@
+import datetime
+
+# This is a comment.
+
+'''
+This
+is 
+a 
+multi-line
+comment.
+'''
+
+# <var> = <data>
+# "=" is the assigment operator
+
+# CHAR
+myChar = "A"
+mySecondChar = 'A'
+
+# STRING
+myGreeting = "Hello, World!"
+myName = "Eishal Keshwani"
+myAddress = 'B-7, Inaara Garden'
+myStr = "Eishal's Bike is Awsome"
+
+# ctrl + /
+
+# INTEGER myNum = 8654
+myNum = 5986354
+myDOB = 23122006
+
+# FLOAT
+myFloat = 2.345
+pi = 3.14
+myFutureFloat = 3.00
+
+# BOOLEAN
+Flag = True
+NotFlag = False
+
+# DATE
+myDOB = datetime.datetime(2006, 12, 23, 3, 15)
