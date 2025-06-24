1382b9db0efbdeb4ac0cee8a3d9c0ae335548e44
Eishal Keshwani <keshwanieishal001@gmail.com>
2025-06-24 21:59:52 +0500

    class02 done

--- classes/Operators.py
+++ classes/Operators.py
@@ -1,45 +1,40 @@
-## Arithmetic Operators
+# ## Arithmetic Operators
 
-a = 7
-b = 2
+# a = 7
+# b = 2
 
-# addition
-print ('Sum: ', a + b)  
+# # addition
+# print ('Sum: ', a + b)  
 
-# subtraction
-print ('Subtraction: ', a - b)   
+# # subtraction
+# print ('Subtraction: ', a - b)   
 
-# multiplication
-print ('Multiplication: ', a * b)  
+# # multiplication
+# print ('Multiplication: ', a * b)  
 
-# division
-print ('Division: ', a / b) 
+# # division
+# print ('Division: ', a / b) 
 
-# floor division
-print ('Floor Division: ', a // b)
+# # floor division
+# print ('Floor Division: ', a // b)
 
-# modulo
-print ('Modulo: ', a % b)  
+# # modulo
+# print ('Modulo: ', a % b)  
 
-# a to the power b
-print ('Power: ', a ** b)
+# # a to the power b
+# print ('Power: ', a ** b)
 
 ## Assignment Operators
 
-# assign 10 to a
-a = 10
+# # assign 10 to a
+# a = 10
 
-# assign 5 to b
-b = 5 
-
-# assign the sum of a and b to a
-a += b      # a = a + b
-
-print(a) # Output 15
+# # assign 5 to b
+# b = 5 
 
 ## Comparison Operators
 
-a = 5
+a = 7
 
 b = 2
 
@@ -61,40 +56,36 @@ print('a >= b =', a >= b)
 # less than or equal to operator
 print('a <= b =', a <= b)
 
-## Logical Operators
+# ## Logical Operators
 
-# logical AND
-print(True and True) # True
-print(True and False) # False
+# # logical AND
+# print(True and True) # True
+# print(True and False) # False
 
-# logical OR
-print(True or False) # True
+# # logical OR
+# print(True or False) # True
 
-# logical NOT
-print(not True) # False
+# # logical NOT
+# print(not True) # False
 
-#Identity Operators
+# #Identity Operators
 
-x1 = 5
-y1 = 5
-x2 = 'Hello'
-y2 = 'Hello'
-x3 = [1,2,3]
-y3 = [1,2,3]
+# x1 = 5
+# y1 = 5
+# x2 = 'Hello'
+# y2 = 'Hello'
 
-print(x1 is not y1)  # prints False
+# print(x1 is not y1)  # prints False
 
-print(x2 is y2)  # prints True
+# print(x2 is y2)  # prints True
 
-print(x3 is y3)  # prints False
+# ## Membership Operators
 
-## Membership Operators
+# message = 'Hello world'
 
-message = 'Hello world'
+# # check if 'H' is present in message string
+# print('H' in message)  # prints True
 
-# check if 'H' is present in message string
-print('H' in message)  # prints True
-
-# check if 'hello' is present in message string
-print('hello' not in message)  # prints True
-# Note: 'hello' is not in 'Hello world' due to case sensitivity
+# # check if 'hello' is present in message string
+# print('hello' not in message)  # prints True
+# # Note: 'hello' is not in 'Hello world' due to case sensitivity
