511a7fa87ce948a19eeb4a393e928272bb513f12
Aka-Ayaan <am08853@st.habib.edu.pk>
2025-06-25 21:05:51 +0500

    operators and iterations

---
+++ classes/Iterations.py
@@ -0,0 +1,29 @@
+## Iterating through a range of numbers
+
+# iterate from i = 0 to i = 3
+for i in range(0, 4):
+    print(i)
+
+## Iterating through a list of strings
+lst1 = ["Intro", "To", "Programming", "With", "Python"]
+
+for i in lst1:
+    print(i)
+
+# Iterating through a string
+str = "Hello"
+
+for i in str:
+    print(i)
+
+## Using break and continue
+
+for i in range(0, 4):
+    if i == 2:
+        continue  # skip the value 2 and print the rest
+    print(i)
+
+for i in range(0, 4):
+    if i == 2:
+        break  # exit the loop when i is 2
+    print(i)
