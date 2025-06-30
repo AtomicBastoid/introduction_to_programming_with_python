c47d236cfe79cf844725905d3b0e9204bee6d22a
Aka-Ayaan <am08853@st.habib.edu.pk>
2025-06-30 20:03:52 +0500

    added while loops

--- classes/Iterations.py
+++ classes/Iterations.py
@@ -27,3 +27,41 @@ for i in range(0, 4):
     if i == 2:
         break  # exit the loop when i is 2
     print(i)
+
+
+## The while loop
+
+number = 1
+
+while number <= 3:
+    print(number)
+    number = number + 1
+
+# Using else statement also works with while loop
+counter = 0
+
+while counter < 2:
+    print("inside loop")
+    counter = counter + 1
+else:
+    print("Outside Loop")
+
+# Print numbers until the user enters 0
+number = int(input('Enter a number: '))
+
+# iterate until the user enters 0
+while number != 0:
+    print('You entered:', number)
+    number = int(input('Enter a number: '))
+
+print('The end.')
+
+# Infinite loop example
+age = 32
+
+while age > 18:
+    print("You are an adult.")
+
+# This is equivalent to:
+while True:
+    print("You are an adult.")
